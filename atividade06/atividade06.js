42799dcf3f2e1ab88ef1e7c989b4e0445a24ae9d
henrique86h <henriqueribeiromiguel86@gmail.com>
2023-11-17 22:21:34 -0300

    pastas e atividade06
    adicionei pastas e ocorreu a realização da atividade06

---
+++ atividade06/atividade06.js
@@ -0,0 +1,21 @@
+function escreveTexto(){
+    document.getElementById("titulo").innerText = "Curso de Javascript"
+    document.getElementById("texto").innerText = "Estou estudando Javascript e espero aprender, ou pelo menos o guto espera que eu aprenda. Mas com o marlon instalando o cs fica dificil o trabalho guto."
+}
+
+function escreveOi(mensagem){
+    document.write(mensagem)
+}
+
+function corDoFundo(cor){
+document.body.style.backgroundColor = cor
+
+}
+    
+
+
+// escreveTexto()
+// corDoFundo("pink")
+// escreveOi("<h1> oiiii </h1>")
+// escreveOi("oeifehu")
+// escreveOi("<h3> olha </h3>")
